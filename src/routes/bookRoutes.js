c799a848ed579c49e14d16893912304632471164
Siddharth Bhal <siddharth0bhal@gmail.com>
2015-12-20 03:03:24 -0500

    mssql can be used. But I am going to use mongodb

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,7 +3,6 @@ var express = require('express');
 var bookRouter = express.Router();
 var router = function(nav) {
 
-
     var books = [{
         title: 'War and Peace',
         genre: 'Historical Fiction',
